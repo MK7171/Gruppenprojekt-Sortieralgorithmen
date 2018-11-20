f5b6c71a1d2e893cee8c5af6f08906f3f9277284
TelekomHacker <45207620+TelekomHacker@users.noreply.github.com>
2018-11-20 18:30:07 +0100

    annotations

--- Sortieren +Vererbung #2.1/InsertionSort.java
+++ Sortieren +Vererbung #2.1/InsertionSort.java
@@ -19,9 +19,9 @@ public class InsertionSort extends Sortieralgorithmen
     void insertionSort(int pArray[], int pN) 
     { 
         array = new int[pN];
-        for(int i=0; i<pArray.length; i++)
+        for(int i=0; i<pArray.length; i++) //solange i kleiner ist als die Laenge von pArray wird i um 1 erhoeht sodass am Ende alle Stellen gleichgesetzt sind
         {
-            array[i] = pArray[i];
+            array[i] = pArray[i]; //Das Array dieser Klasse wird dem der anderen gleichgesetzt
         }
         meineUhr.starte();
         for (int i=1; i<pN; ++i) 
