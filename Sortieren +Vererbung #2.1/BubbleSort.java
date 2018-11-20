c2bda8fda6513031aa2dc275e023a0f6345fbf75
Derrnux <45166410+Derrnux@users.noreply.github.com>
2018-11-20 16:55:10 +0100

    Zeit ist nun mittelwert
    von 10 mal sortieren

--- Sortieren +Vererbung #2.1/BubbleSort.java
+++ Sortieren +Vererbung #2.1/BubbleSort.java
@@ -47,4 +47,21 @@ public class BubbleSort extends Sortieralgorithmen
         meineUhr.stoppe();
         t = meineUhr.gestoppteZeit();
     }
+        /*Gibt Wert der gestoppten Zeit zurück
+         * als mittelwert von 10 sortierungen
+    */ 
+    public double time(int array[] ,int  n)
+        {
+             
+            for(int i=0; i<9 ; i++) //i index
+            {
+                f = f + t;   //die neuen Zahlen werden addiert
+                this.bubbleSort(array, n);      //erneutes Sortieren
+            }
+            
+            t = f / 10; //Mittlere Wert wird berechnet
+            f = 0;
+            return (t);  //mittlerer Wert wird zurück gegeben
+            
+        }
 } 
