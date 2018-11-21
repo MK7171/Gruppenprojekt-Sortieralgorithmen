1463d792bd139dab4e4a9f70fe2945bb8bee399e
TelekomHacker <45207620+TelekomHacker@users.noreply.github.com>
2018-11-21 19:33:57 +0100

    kommentare update

--- Sortieren +Vererbung #2.1/BubbleSort.java
+++ Sortieren +Vererbung #2.1/BubbleSort.java
@@ -20,28 +20,28 @@ public class BubbleSort extends Sortieralgorithmen
     public void bubbleSort(int pArray[], int pN)
     {
         array = new int[pN];
-        for(int i=0; i<pArray.length; i++) //solange i kleiner ist als die Laenge von pArray wird i um 1 erhoeht sodass am Ende alle Stellen gleichgesetzt sind
+        for(int i=0; i<pArray.length; i++) // Solange i kleiner ist als die Laenge von pArray wird i um 1 erhoeht sodass am Ende alle Stellen gleichgesetzt sind
         {
-            array[i] = pArray[i]; //Das Array dieser Klasse wird dem der anderen gleichgesetzt
+            array[i] = pArray[i]; // Das Array dieser Klasse wird dem der anderen gleichgesetzt
         }
         
         meineUhr.starte();
         
-        for(int w=0; w<pN-1; w++) //w wiederholungen
+        for(int w=0; w<pN-1; w++) // w wiederholungen
         {
             swapped = false;
-            for(int i=0; i<pN-1 ; i++) //i index
+            for(int i=0; i<pN-1 ; i++) // i index
             {
                 if(array[i+1]<array[i])
                 {
                     int temporary = array[i];
                     array[i] = array[i+1];
                     array[i+1] = temporary;
-                    println(); //Ausgeben von aktuellem Array
+                    println(); // Ausgeben von aktuellem Array
                     swapped = true;
                 }
             }
-            if(!swapped) //Damit früher beendete Sortierung festgestellt werden kann (Zeitkürzung)
+            if(!swapped) // Damit früher beendete Sortierung festgestellt werden kann (Zeitkürzung)
             {break;}
         }
         meineUhr.stoppe();
@@ -53,16 +53,16 @@ public class BubbleSort extends Sortieralgorithmen
     public double time(int array[] ,int  n)
         {
              
-            for(int i=0; i<9 ; i++) //i index
+            for(int i=0; i<9 ; i++) // i index
             {
                 this.deleteln();
-                f = f + t; //die neuen Zahlen werden addiert
-                this.bubbleSort(array, n); //erneutes Sortieren
+                f = f + t; // Die neuen Zahlen werden addiert
+                this.bubbleSort(array, n); // Erneutes Sortieren
             }
             
-            t = f / 10; //Mittlerer Wert wird berechnet
+            t = f / 10; // Mittlerer Wert wird berechnet
             f = 0;
-            return (t); //Mittlerer Wert wird zurück gegeben
+            return (t); // Mittlerer Wert wird zurück gegeben
             
         }
 } 
