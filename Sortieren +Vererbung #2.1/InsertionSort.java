241e2686b9d0f335a5fb884d93fc39805ea004f2
TelekomHacker <45207620+TelekomHacker@users.noreply.github.com>
2018-11-21 19:27:13 +0100

    kommentare update

--- Sortieren +Vererbung #2.1/InsertionSort.java
+++ Sortieren +Vererbung #2.1/InsertionSort.java
@@ -23,50 +23,50 @@ public class InsertionSort extends Sortieralgorithmen
         {
             array[i] = pArray[i]; //Das Array dieser Klasse wird dem der anderen gleichgesetzt
         }
-        meineUhr.starte();          //startet die Stoppuhr
-        /*Die For-Schleife läuft einmal weniger durch als das Array lang ist. Dies kommt daher, dass jede Position genau 
-        einmal mit den davor liegenden verglichen wird außer die erste. Bei einem Array der länge 28 läuft sie also 9 mal durch
-        */
+        meineUhr.starte(); //startet die Stoppuhr
+        /* Die For-Schleife läuft einmal weniger durch als das Array lang ist. Dies kommt daher, dass jede Position genau 
+           einmal mit den davor liegenden verglichen wird außer die erste. Bei einem Array der länge 28 läuft sie also 9 mal durch */
+        
         for (int i=1; i<pN; i++) 
         { 
             int key = array[i]; 
             int j = i-1; 
-                /*Vergleicht zwei positionen des Arrays miteinander. Bsw.Position 5 mit 4.
-                Also zuerst immer eine mit der davor liegenden. Wenn die Davorliegende größer ist
-                 das Vergleichsobjekt, dann wird das nächste verglichen (in diesem Fall die Position 3).
-                 Wenn diese nun auch größer ist wird wieder die Position davor genommen. Das geschiet bis das 
-                 Vergleichsobjekt kleiner ist als die Position an der man gerade ist oder man mit der 1ten Position 
-                 des Arrays verglichen hat. Dann wird das Vergleichsobjekt hinter das kleinere gelegt und alle anderen rücken auf
-                */
+            /* Vergleicht zwei positionen des Arrays miteinander. Bsw.Position 5 mit 4.
+               Also zuerst immer eine mit der davor liegenden. Wenn die Davorliegende größer ist
+               das Vergleichsobjekt, dann wird das nächste verglichen (in diesem Fall die Position 3).
+               Wenn diese nun auch größer ist wird wieder die Position davor genommen. Das geschiet bis das 
+               Vergleichsobjekt kleiner ist als die Position an der man gerade ist oder man mit der 1ten Position 
+               des Arrays verglichen hat. Dann wird das Vergleichsobjekt hinter das kleinere gelegt und alle anderen rücken auf */
+                
             while (j>=0 && array[j] > key)  
             { 
                 array[j+1] = array[j]; 
                 j = j-1; 
             } 
             array[j+1] = key; 
-            println(); //ausgeben von aktuellem Array
+            println(); //Ausgeben von aktuellem Array
         } 
-        meineUhr.stoppe();      //stoppt die Stoppuhr
+        meineUhr.stoppe(); //stoppt die Stoppuhr
         t = meineUhr.gestoppteZeit();
     }
     
     
-        /*Gibt Wert der gestoppten Zeit zurück
-         * als mittelwert von 10 sortierungen
-        */ 
+    /* Gibt Wert der gestoppten Zeit zurück
+       als mittelwert von 10 sortierungen */
+         
     public double time(int array[] ,int  n)
         {
              
             for(int i=0; i<9 ; i++) //i index
             {
                 this.deleteln();
-                f = f + t;   //die neuen Zahlen werden addiert
-                this.insertionSort(array, n);      //erneutes Sortieren
+                f = f + t; //Die neuen Zahlen werden addiert
+                this.insertionSort(array, n); //erneutes Sortieren
             }
             
-            t = f / 10; //Mittlere Wert wird berechnet
+            t = f / 10; //Mittlerer Wert wird berechnet
             f = 0;
-            return (t);  //mittlerer Wert wird zurück gegeben
+            return (t); //Mittlerer Wert wird zurück gegeben
             
         }
 
